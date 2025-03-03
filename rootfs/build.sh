cda607ddc73f0a2b385fb4cc3eecffefbf0eae52
NN708 <nn708@outlook.com>
2025-03-03 08:04:28 +0800

    feat: split debootstrap into two stages

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -8,17 +8,9 @@ REPO_URL=http://repo2.vanillaos.org
 CUSTOM_PACKAGE=""
 CLEANUP_DIRS="/usr/share/doc/*
 /usr/share/info/*
-/usr/share/linda/*
 /usr/share/lintian/overrides/*
 /usr/share/locale/*
-/usr/share/man/*
-/usr/share/doc/kde/HTML/*/*
-/usr/share/gnome/help/*/*
-/usr/share/locale/*
-/usr/share/omf/*/*-*.emf
-/var/cache
-/var/log/*
-/tmp/*"
+/usr/share/man/*"
 
 # Root check - debootstrap requires root privileges
 if [ "$(id -u)" != "0" ];
@@ -45,6 +37,7 @@ debootstrap \
     --variant=minbase \
     --include=$CUSTOM_PACKAGE,apt-utils,apt-transport-https,ca-certificates,gnupg2,bash,bzip2 \
     --keyring=includes.rootfs/usr/share/keyrings/vanilla_keyring.gpg \
+    --foreign \
     sid \
     $ROOTFS_NAME \
     $REPO_URL
@@ -55,18 +48,9 @@ debootstrap \
 rm -rf $ROOTFS_NAME/etc/apt/sources.list
 
 # Cleanup
-chroot $ROOTFS_NAME apt update
-chroot $ROOTFS_NAME apt upgrade -y
-chroot $ROOTFS_NAME apt install -f -y
-chroot $ROOTFS_NAME apt clean
-chroot $ROOTFS_NAME apt autoremove -y
-
 for dir in $CLEANUP_DIRS; do
     rm -rf $ROOTFS_NAME/$dir
 done
-
-# Restore apt cache directory
-mkdir -p $ROOTFS_NAME/var/cache/apt/archives/partial
 
 # Compression
 tar -cvf $ROOTFS_NAME.tar -C $ROOTFS_NAME .
