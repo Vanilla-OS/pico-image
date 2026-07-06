0aa0a7087c16b188109419dca29dfaea027af05a
NN708 <nn708@outlook.com>
2026-07-06 23:42:05 +0000

    chore: update repo to 20260706T022345Z

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -4,7 +4,7 @@ set -e
 
 # Rootfs definitions
 ROOTFS_NAME="vanilla-pico"
-REPO_URL=https://repo3.vanillaos.org/20260528T022539Z
+REPO_URL=https://repo3.vanillaos.org/20260706T022345Z
 CUSTOM_PACKAGE=""
 CLEANUPS=(
   "/usr/share/doc/*"
